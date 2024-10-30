310d3faf06391aa0ca087288647486a13efde14a
armanyrs <83285330+armanyrs@users.noreply.github.com>
2024-10-30 09:06:27 +0700

    Update index_xr.js

--- Bab 6/index_xr.js
+++ Bab 6/index_xr.js
@@ -1,4 +1,4 @@
-import * as THREE from './three.module.js';
+import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r118/three.module.min.js";
 import { VRButton } from './VRButton.js';
 
 var gl, cube, sphere, light, camera, scene;
